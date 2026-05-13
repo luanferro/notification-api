d497102882374fc426e7033f388c5821ff65f97f
luanferro <luanferror@gmail.com>
2026-05-13 10:57:45 -0300

    feat: add json creator to enums to accept lowercase values

--- src/main/java/com/luanferro/notification_api/entity/enums/NotificationStatus.java
+++ src/main/java/com/luanferro/notification_api/entity/enums/NotificationStatus.java
@@ -1,7 +1,14 @@
 package com.luanferro.notification_api.entity.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum NotificationStatus {
     PENDING,
     SENT,
-    FAILED
+    FAILED;
+
+    @JsonCreator
+    public static NotificationStatus fromString(String value){
+        return NotificationStatus.valueOf(value.toUpperCase());
+    }
 }
