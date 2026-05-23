7da90f5fd7205fb049fb6bd38758715829922892
luanferro <luanferror@gmail.com>
2026-05-23 14:23:41 -0300

    feat: add config to DLQ queue

--- src/main/java/com/luanferro/notification_api/config/RabbitMQConfig.java
+++ src/main/java/com/luanferro/notification_api/config/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.luanferro.notification_api.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +12,24 @@ public class RabbitMQConfig {
     public static final String EXCHANGE = "notification.exchange";
     public static final String ROUTING_KEY = "notification.routingKey";
 
+    public static final String DLQ = "notification.dlq";
+    public static final String DLQ_EXCHANGE = "notification.dlq.exchange";
+    public static final String DLQ_ROUTING_KEY = "notification.dlq.routingKey";
+
     @Bean
     public Queue queue(){
         return  QueueBuilder
                     .durable(QUEUE)
                     .withArgument("x-max-priority", 10)
+                    .withArgument("x-dead-letter-exchange", DLQ_EXCHANGE)
+                    .withArgument("x-dead-letter-routing-key", DLQ_ROUTING_KEY)
+                    .build();
+    }
+
+    @Bean
+    public Queue queueDLQ(){
+        return QueueBuilder
+                    .durable(DLQ)
                     .build();
     }
 
@@ -25,10 +39,29 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Binding binding(Queue queue, DirectExchange exchange){
+    public DirectExchange exchangeDLQ() {
+        return new DirectExchange(DLQ_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(
+            @Qualifier("queue") Queue queue,
+            @Qualifier("exchange") DirectExchange exchange
+    ){
         return BindingBuilder
                 .bind(queue)
                 .to(exchange)
                 .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bindingDLQ(
+            @Qualifier("queueDLQ") Queue queueDLQ,
+            @Qualifier("exchangeDLQ") DirectExchange exchangeDLQ
+    ) {
+        return BindingBuilder
+                .bind(queueDLQ)
+                .to(exchangeDLQ)
+                .with(DLQ_ROUTING_KEY);
     }
 }
